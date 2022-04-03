c5bb19316937093088926baa442a6c04ea5158c5
Carlos Arellano <36318447+elCarlosHez@users.noreply.github.com>
2022-04-03 15:39:39 -0500

    [Feature] Basic Views UI (#1)
    * feature: Basic UI configuration
    - Colors
    - Strings
    - Theme
    
    * feature: Main activity
    feature: Take picture instructions
    feature: Successful Register Tree
    
    * bugfix: Fixed last view hidden content
    
    * refactor: Github comments

---
+++ app/src/main/java/com/three/tree_training_app/SuccessfulRegisterTree.kt
@@ -0,0 +1,11 @@
+package com.three.tree_training_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SuccessfulRegisterTree : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_successful_register_tree)
+    }
+}
