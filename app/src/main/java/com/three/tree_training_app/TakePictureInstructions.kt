7b30ff200107cef65f410e23f4f9c780c9fd0f9a
Carlos Arellano <luis.harellano@alumnos.udg.mx>
2022-04-05 12:45:39 -0500

    feat: taking picture of a tree using an external app
    For now, we send the uri to the next screen

--- app/src/main/java/com/three/tree_training_app/TakePictureInstructions.kt
+++ app/src/main/java/com/three/tree_training_app/TakePictureInstructions.kt
@@ -1,13 +1,46 @@
 package com.three.tree_training_app
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.three.tree_training_app.databinding.ActivityMainBinding
+import android.os.Environment
+import android.provider.MediaStore
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import com.three.tree_training_app.databinding.ActivityTakePictureInstructionsBinding
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class TakePictureInstructions : AppCompatActivity() {
     private lateinit var binding: ActivityTakePictureInstructionsBinding
+
+    private var latestTmpUri: Uri? = null
+
+    private val takeImageResult = registerForActivityResult(ActivityResultContracts.TakePicture()) { isSuccess ->
+        if (isSuccess) {
+            latestTmpUri?.let { uri ->
+                var intent = Intent(this, FillTreeInfo::class.java)
+                intent.putExtra("img",uri.toString())
+                startActivity(intent)
+            }
+        }
+    }
+
+    private fun getTmpFileUri(): Uri {
+        val tempName: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val tmpFile = File.createTempFile(tempName, ".png", cacheDir).apply {
+            createNewFile()
+            //@TODO: Determinate if its necessary to delete the image
+            deleteOnExit()
+        }
+
+        return FileProvider.getUriForFile(applicationContext, "${BuildConfig.APPLICATION_ID}.provider", tmpFile)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,8 +48,10 @@ class TakePictureInstructions : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.imageButton.setOnClickListener {
-            var intent = Intent(this, SuccessfulRegisterTree::class.java)
-            startActivity(intent)
+            getTmpFileUri().let { uri ->
+                latestTmpUri = uri
+                takeImageResult.launch(uri)
+            }
         }
     }
 }
