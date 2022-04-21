b45717c445e8180ffcaf4c1f493d7a2f5e4275cb
Carlos Arellano <36318447+elCarlosHez@users.noreply.github.com>
2022-04-21 09:11:18 -0500

    [Feature]: Filled the tree info view (#3)
    * feat: UI for filled the tree info
    
    * feat!: Added Request User location and user it inside a fragment map
    refactor: Fix FillTreeInfo layout

--- app/src/main/java/com/three/tree_training_app/FillTreeInfoActivity.kt
+++ app/src/main/java/com/three/tree_training_app/FillTreeInfoActivity.kt
@@ -1,10 +1,10 @@
 package com.three.tree_training_app
 
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.three.tree_training_app.databinding.ActivityFillTreeInfoBinding
-import com.three.tree_training_app.databinding.ActivityTakePictureInstructionsBinding
+
 
 class FillTreeInfoActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFillTreeInfoBinding
